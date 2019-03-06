a7d57cf15f46fcfb8a90a483096b6d58f44f9823
Ali Najafi <39293965+AliNajafi1998@users.noreply.github.com>
2019-03-06 18:08:33 +0330

    Ui

---
+++ newUniversity/LoginPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace newUniversity
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+
+        public LoginPage()
+        {
+            InitializeComponent();
+            ComboBoxItem ComboItem = (ComboBoxItem)comboBox.SelectedItem;
+
+
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin(object sender, RoutedEventArgs e)
+        {
+
+            if (comboBox.SelectionBoxItem.ToString().Equals("Manager"))
+            {
+
+                var newForm = new MainWindow(); //create your new form.
+                newForm.Show(); //show the new form.
+                this.Close(); //only if you want to close the current form.
+            }
+            else if (comboBox.SelectionBoxItem.ToString().Equals("Master"))
+            {
+
+            }
+            else if (comboBox.SelectionBoxItem.ToString().Equals("Student"))
+            {
+
+            }
+        }
+    }
+}
---
+++ newUniversity/ManagerWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace newUniversity
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+    
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+        
+        
+    }
+}
