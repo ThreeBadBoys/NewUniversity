6dc919dbc332cb1be6500661e74210ee180ac789
Ali Najafi <39293965+AliNajafi1998@users.noreply.github.com>
2019-03-06 21:20:46 +0330

    LoadingPage
    LoadingPage is completed

---
+++ newUniversity/LoadingPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace newUniversity
+{
+    /// <summary>
+    /// Interaction logic for LoadingPage.xaml
+    /// </summary>
+    public partial class LoadingPage : Window , IDisposable
+    {
+        public Action Worker { get; set; }
+        public LoadingPage(Action worker)
+        {
+            InitializeComponent();
+            Worker = worker;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(Worker).ContinueWith(t => { Close(); }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        public void Dispose() {}
+    }
+}
--- newUniversity/LoginPage.xaml.cs
+++ newUniversity/LoginPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -23,11 +25,10 @@ namespace newUniversity
         public LoginPage()
         {
             InitializeComponent();
-            ComboBoxItem ComboItem = (ComboBoxItem)comboBox.SelectedItem;
-
-
         }
-
+        string id = "1234";
+        string password = "1234";
+        BlurEffect myEffect = new BlurEffect();
         private void ButtonClose_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
@@ -35,22 +36,56 @@ namespace newUniversity
 
         private void btnLogin(object sender, RoutedEventArgs e)
         {
-
             if (comboBox.SelectionBoxItem.ToString().Equals("Manager"))
             {
+                MyBlureEffect();
+                if (passwordInput.Password.ToString().Equals(password))
+                {
 
-                var newForm = new MainWindow(); //create your new form.
-                newForm.Show(); //show the new form.
-                this.Close(); //only if you want to close the current form.
+
+
+                    var newForm = new MainWindow(); //create your new form.
+                    newForm.Show(); //show the new form.
+                    this.Close(); //only if you want to close the current form.
+                }
+                else
+                {
+
+                }
+
             }
             else if (comboBox.SelectionBoxItem.ToString().Equals("Master"))
             {
+                MyBlureEffect();
 
             }
             else if (comboBox.SelectionBoxItem.ToString().Equals("Student"))
             {
-
+                MyBlureEffect();
             }
         }
+        void Simulator()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Thread.Sleep(7);
+            }
+        }
+     
+        void MyBlureEffect()
+        {
+            myEffect.Radius = 10;
+            Effect = myEffect;
+
+            using (LoadingPage lw = new LoadingPage(Simulator))
+            {
+                lw.Owner = this;
+                lw.ShowDialog();
+            }
+            myEffect.Radius = 0;
+            Effect = myEffect;
+
+        }
+
     }
 }
