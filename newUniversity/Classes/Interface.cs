1c0a2269dd4651e331f991b342baf2d74192910a
Ali Najafi <39293965+AliNajafi1998@users.noreply.github.com>
2019-03-16 19:55:42 +0330

    namespace updated

--- newUniversity/Classes/Interface.cs
+++ newUniversity/Classes/Interface.cs
@@ -8,12 +8,19 @@ namespace newUniversity.Classes
 {
     class Interface
     {
+        static University uni;
 
         public static bool authentication(string usertype,string userName , string password)
         {
+            
+            if(usertype.Equals("Manager"))
+            {
+                University.instance.managers.getByID(Convert.ToInt32(userName));
+                
 
 
-
+            }
+           
             return true;
         }
     }
--- newUniversity/Classes/Node.cs
+++ newUniversity/Classes/Node.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace University.Classes
+namespace newUniversity.Classes
 {
 
     // helper B-tree node data type
