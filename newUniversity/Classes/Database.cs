5d091ec32ff077b6c5618df536f31e7a5d8e01d1
Ali Najafi <39293965+AliNajafi1998@users.noreply.github.com>
2019-03-17 10:59:43 +0330

    Revert "newUnivesity -> University on name spaces"
    This reverts commit 4872ee464cfe2b9d59981899d1bdf9a81e97279c.

--- newUniversity/Classes/Database.cs
+++ newUniversity/Classes/Database.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
-namespace University.Classes
+namespace newUniversity.Classes
 {
     abstract class Database
     {
--- newUniversity/Classes/ManagerDB.cs
+++ newUniversity/Classes/ManagerDB.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace University.Classes
+namespace newUniversity.Classes
 {
     class ManagerDB : UserDB
     {
