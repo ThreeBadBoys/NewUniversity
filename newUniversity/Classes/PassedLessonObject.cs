c142d04899830d4a514aa3b804e30c230ee63c01
Mohammad-adk <mohamad.adk78@gmail.com>
2019-04-01 13:14:09 +0430

    passedLessonObject.cs modified

--- newUniversity/Classes/PassedLessonObject.cs
+++ newUniversity/Classes/PassedLessonObject.cs
@@ -7,7 +7,7 @@
         public PassedLessonObject(string title, short unitsCount, int masterID, int grade)
         {
             this.title = title;
-            this.Name = title;
+            this.name = title;
             this.unitsCount = unitsCount;
             this.masterID = masterID;
             this.grade = grade;
