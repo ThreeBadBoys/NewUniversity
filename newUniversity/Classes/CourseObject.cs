a7657bef3d13aa74dcc5fc8820b9558b898ba244
Mohammad-adk <mohamad.adk78@gmail.com>
2019-04-10 03:06:42 +0430

    CourseObject attributes updated

--- newUniversity/Classes/CourseObject.cs
+++ newUniversity/Classes/CourseObject.cs
@@ -7,11 +7,111 @@ namespace newUniversity.Classes
   public  class CourseObject : LessonObject
     {
         public List<int> studentsID;
-        public string examDate;
-        public string examTime;
-        public string courseTime;
-        public string classDays;
+        public char[] ExamDate = new char[10];
+        public char[] ExamTime = new char[5];
+        public char[] CourseTime = new char[20];
+        public char[] ClassDays = new char[20];
 
+
+        public string examDate
+        {
+            get
+            {
+                string temp = "";
+                for (int i = 0; i < ExamDate.Length; i++)
+                {
+                    if (ExamDate[i] != '\0')
+                        temp += ExamDate[i];
+                    else
+                        break;
+                }
+                return temp;
+            }
+            set
+            {
+                int i;
+                for (i = 0; i < value.Length; i++)
+                {
+                    ExamDate[i] = value[i];
+                }
+                ExamDate[i] = '\0';
+            }
+        }
+
+        public string examTime
+        {
+            get
+            {
+                string temp = "";
+                for (int i = 0; i < ExamTime.Length; i++)
+                {
+                    if (ExamTime[i] != '\0')
+                        temp += ExamTime[i];
+                    else
+                        break;
+                }
+                return temp;
+            }
+            set
+            {
+                int i;
+                for (i = 0; i < value.Length; i++)
+                {
+                    ExamTime[i] = value[i];
+                }
+                ExamTime[i] = '\0';
+            }
+        }
+
+        public string courseTime
+        {
+            get
+            {
+                string temp = "";
+                for (int i = 0; i < CourseTime.Length; i++)
+                {
+                    if (CourseTime[i] != '\0')
+                        temp += CourseTime[i];
+                    else
+                        break;
+                }
+                return temp;
+            }
+            set
+            {
+                int i;
+                for (i = 0; i < value.Length; i++)
+                {
+                    CourseTime[i] = value[i];
+                }
+                CourseTime[i] = '\0';
+            }
+        }
+
+        public string classDays
+        {
+            get
+            {
+                string temp = "";
+                for (int i = 0; i < ClassDays.Length; i++)
+                {
+                    if (ClassDays[i] != '\0')
+                        temp += ClassDays[i];
+                    else
+                        break;
+                }
+                return temp;
+            }
+            set
+            {
+                int i;
+                for (i = 0; i < value.Length; i++)
+                {
+                    ClassDays[i] = value[i];
+                }
+                ClassDays[i] = '\0';
+            }
+        }
         public CourseObject(int id,  string title, short unitsCount, int masterID, string examDate, string examTime, string courseTime, string classDays)
         {
             ID = id;
